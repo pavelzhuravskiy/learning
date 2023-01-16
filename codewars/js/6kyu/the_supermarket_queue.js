6b0a01e969fc098d31c58e56d4715f2451eef916
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-01-16 12:35:16 +0200

    Added kata

---
+++ codewars/js/6kyu/the_supermarket_queue.js
@@ -0,0 +1,17 @@
+const queueTime = (customers, n) => {
+  if (customers.length === 0) {
+    return 0;
+  } else if (n === 1) {
+    return customers.reduce((a, b) => a + b, 0);
+  } else if (n < customers.length) {
+    const firstQueue = customers.slice(0, n);
+    const secondQueue = customers.slice(n, customers.length);
+    for (let i = 0; i < secondQueue.length; i++) {
+      const minIndex = firstQueue.indexOf(Math.min(...firstQueue));
+      firstQueue[minIndex] = firstQueue[minIndex] + secondQueue[i];
+    }
+    return Math.max(...firstQueue);
+  } else {
+    return Math.max(...customers);
+  }
+};
